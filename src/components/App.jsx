25fa185bc9e301633a63b392faf3ed3613c6e3ca
Yannick Lohse <contact@yannick-lohse.fr>
2018-02-22 19:27:30 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = ({ children }) => (
+  <div className="app-wrapper c-layout">
+    <main className='app-content'>
+    </main>
+  </div>
+)
+
+export default App
